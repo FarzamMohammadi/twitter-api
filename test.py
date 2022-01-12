56ed956880d5904b3f980974d248d1a4f0121e25
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-01-12 16:00:59 -0500

    unit tests for login and registration api complete

--- test.py
+++ test.py
@@ -9,21 +9,34 @@ conn.commit()
 
 
 # 'step' added to functions names to ensure test runs in desired order
-class RegisterTestCases(unittest.TestCase):
+class Step1RegisterTestCases(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def step1_test_registration_invalid_password(self):
-        response = self.app.post('/register/farzamMoTest/farzam123', headers={"Content-Type": "application/json"})
+    def test_step1_registration_invalid_password(self):
+        response = self.app.post('/register/farzamMoTest/farzam123')
         self.assertEqual(500, response.status_code)
 
-    def step2_test_registration_valid_password(self):
-        response = self.app.post('/register/farzamMoTest/Farzam123@', headers={"Content-Type": "application/json"})
+    def test_step2_registration_valid_password(self):
+        response = self.app.post('/register/farzamMoTest/Farzam123@')
         self.assertEqual(201, response.status_code)
 
-    def step3_test_existing_user(self):
-        response = self.app.post('/register/farzamMoTest/Farzam123@', headers={"Content-Type": "application/json"})
+    def test_step3_existing_user(self):
+        response = self.app.post('/register/farzamMoTest/Farzam123@')
         self.assertEqual(409, response.status_code)
+
+
+class Step2LoginTestCases(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_step4_invalid_credentials(self):
+        response = self.app.post('/login/farzamMoTest/farzam123')
+        self.assertEqual(401, response.status_code)
+
+    def test_step5_valid_credentials(self):
+        response = self.app.post('/login/farzamMoTest/Farzam123@')
+        self.assertEqual(200, response.status_code)
 
 
 if __name__ == '__main__':
