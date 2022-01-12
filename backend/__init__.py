88c16254d7361d3921f71ba84f8aace2bc1e7a2e
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-01-12 10:02:11 -0500

    registration api password validation complete

--- backend/__init__.py
+++ backend/__init__.py
@@ -28,12 +28,47 @@ def insert_new_user(username, password):
         pass
 
 
+def password_check(passowrd):
+
+    SpecialSym = ['$', '@', '#', '%', '/']
+    val = True
+
+    if len(passowrd) < 6:
+        print('length should be at least 6')
+        val = False
+
+    if len(passowrd) > 20:
+        print('length should be not be greater than 8')
+        val = False
+
+    if not any(char.isdigit() for char in passowrd):
+        print('Password should have at least one numeral')
+        val = False
+
+    if not any(char.isupper() for char in passowrd):
+        print('Password should have at least one uppercase letter')
+        val = False
+
+    if not any(char.islower() for char in passowrd):
+        print('Password should have at least one lowercase letter')
+        val = False
+
+    if not any(char in SpecialSym for char in passowrd):
+        print('Password should have at least one of the symbols $@#')
+        val = False
+    if val:
+        return val
+
+
 class Registration(Resource):
     create_users_table()
 
     def post(self, username, passowrd):
-        insert_new_user(username, passowrd)
-        return {'username': username}, 200
+        if password_check(passowrd):
+            insert_new_user(username, passowrd)
+            return {'username': username}, 200
+        else:
+            return {'error': 'Invalid Password'}, 500
 
 
 api.add_resource(Registration, '/register/<string:username>/<string:passowrd>')
